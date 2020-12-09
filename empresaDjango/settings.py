567fa423a7397a2064e5fde6bd98dd186b8bba52
dbujan <david.bujan@deusto.es>
2020-12-09 12:30:04 +0100

    settings.py

--- empresaDjango/settings.py
+++ empresaDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_!(7ap^v!5m8dvcw0p6*h6(rqfydvzcla&dw%_=(0qmw@3ns)f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
