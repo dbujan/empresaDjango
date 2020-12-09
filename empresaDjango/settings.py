fc630d7be832b2c1ae44001055dc8d64afc4215d
dbujan <david.bujan@deusto.es>
2020-12-09 13:11:09 +0100

    Update settings.py

--- empresaDjango/settings.py
+++ empresaDjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 from pathlib import Path
 
@@ -142,4 +142,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
