41c06d25732bef64daa6b4c3459aec32d1abc68c
David Buján Carballal <david.bujan@deusto.es>
2024-11-06 22:13:44 +0100

    Update settings.py

--- empresaDjango/settings.py
+++ empresaDjango/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'empresaDjango.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5433/empresaDjangoDB',
+        default='postgresql://postgres:postgres@localhost:5432/empresadjangodb',
         conn_max_age=600
     )
 }
@@ -128,12 +128,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR / '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
