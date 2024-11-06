6a9094b51044802b49f220fe5793759d8217de36
David Buján Carballal <david.bujan@deusto.es>
2024-11-06 23:26:21 +0100

    Portada

--- appEmpresaDjango/views.py
+++ appEmpresaDjango/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render
 from .models import Departamento, Habilidad, Empleado
-from django.db.models import Max
+from django.db.models import OuterRef, Subquery, Max
 
 #devuelve el listado de empresas
 
 def home(request):
 	departamentos = get_list_or_404(Departamento.objects.order_by('nombre'))
+	
 	#empleadosFiltrados = Empleado.objects.raw('SELECT * FROM( SELECT * FROM appEmpresaDjango_empleado ORDER BY antiguedad DESC) GROUP BY departamento_id ')
-	empleadosFiltrados = ( Empleado.objects.annotate(max_antiguedad=Max('antiguedad')).order_by('departamento_id', '-antiguedad').distinct('departamento_id') )
+	#empleadosFiltrados = ( Empleado.objects.annotate(max_antiguedad=Max('antiguedad')).order_by('departam7ento_id', '-antiguedad').distinct('departamento_id') )
+	
+	# Obtener la antigüedad máxima por cada departamento
+	max_antiguedad_por_departamento = Empleado.objects.filter(departamento_id=OuterRef('departamento_id')).order_by().values('departamento_id').annotate(max_antiguedad=Max('antiguedad')).values('max_antiguedad')
+	# Filtrar empleados que tengan la antigüedad máxima en su departamento
+	empleadosFiltrados = Empleado.objects.filter(antiguedad__in=max_antiguedad_por_departamento).order_by('departamento_id', '-antiguedad')
+
 	context = {'empleadosFiltrados': empleadosFiltrados }
 	return render(request, 'home.html', context)
 
